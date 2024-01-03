bb26bacbf60eeaaa3b97cc487cac48e847c92d87
Jaesung Lee <chic0815@konkuk.ac.kr>
2024-01-04 03:13:00 +0900

    [추가] 검증 스크립트 추가

---
+++ .github/scripts/GUARD_COMMITS.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TARGET_FILES=("swift-cinema/Sources/CinemaUI/MovieList.swift" "swift-cinema/Sources/CinemaUI/MovieRow.swift" "swift-cinema/Sources/CinemaUI/SubmissionDetailView.swift" "swift-cinema/Sources/Fetcher/Responses.swift" "swift-cinema/Tests/FetcherTests/MovieCollectionFetcherTests.swift" ".github/workflows/REVIEW_CODE.yml")
+
+COMMIT_COUNT=$(git rev-list --count HEAD)
+
+if [ $COMMIT_COUNT -gt 1 ]; then
+  CHANGED_FILES=$(git diff --name-only HEAD~1 HEAD)
+
+  for TARGET_FILE in "${TARGET_FILES[@]}"; do
+    if [[ $CHANGED_FILES == *"$TARGET_FILE"* ]]; then
+      echo "에러! 수정하면 안되는 파일에 수정이 발생했습니다. 파일경로: $TARGET_FILE"
+      exit 1
+    fi
+  done
+fi
+
+echo "테스트가 가능합니다."
